a92c9774c0cff2fd2c4e9923922f5ff11266252c
pabercrombie <pabercrombie@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2012-06-06 19:24:42 +0000

    Update Version class to 1.3.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@629 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/Version.java
+++ WorldWind/src/gov/nasa/worldwind/Version.java
@@ -13,10 +13,10 @@ package gov.nasa.worldwind;
 public class Version
 {
     private static final String MAJOR_VALUE = "1";
-    private static final String MINOR_VALUE = "2";
+    private static final String MINOR_VALUE = "3";
     private static final String DOT_VALUE = "0";
     private static final String versionNumber = MAJOR_VALUE + "." + MINOR_VALUE + "." + DOT_VALUE;
-    private static final String versionName = "NASA World Wind Java 1.2";
+    private static final String versionName = "NASA World Wind Java 1.3";
 
     public static String getVersion()
     {
