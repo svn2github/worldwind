ac4ee217fec31834dc45ebc5c1387a95b1bc15ed
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2012-09-26 01:47:35 +0000

    Reorganized the methods in PickSupport to improve its readability. Removed the method PickSupport.areSelectionsTheSame, which is unused in both World Wind Java and World Wind Android.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@805 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindAndroidSDK/WorldWindAndroid/src/gov/nasa/worldwind/pick/PickSupport.java
+++ WorldWindAndroidSDK/WorldWindAndroid/src/gov/nasa/worldwind/pick/PickSupport.java
@@ -1,8 +1,7 @@
-/*
- * Copyright (C) 2012 United States Government as represented by the Administrator of the
- * National Aeronautics and Space Administration.
- * All Rights Reserved.
- */
+/* Copyright (C) 2001, 2012 United States Government as represented by
+the Administrator of the National Aeronautics and Space Administration.
+All Rights Reserved.
+*/
 package gov.nasa.worldwind.pick;
 
 import android.graphics.Point;
@@ -24,19 +23,9 @@ public class PickSupport
     {
     }
 
-    public void clearPickList()
+    public void addPickableObject(PickedObject po)
     {
-        this.getPickableObjects().clear();
-    }
-
-    public void addPickableObject(int colorCode, Object o, Position position, boolean isTerrain)
-    {
-        this.getPickableObjects().put(colorCode, new PickedObject(colorCode, o, position, isTerrain));
-    }
-
-    public void addPickableObject(int colorCode, Object o, Position position)
-    {
-        this.getPickableObjects().put(colorCode, new PickedObject(colorCode, o, position, false));
+        this.getPickableObjects().put(po.getColorCode(), po);
     }
 
     public void addPickableObject(int colorCode, Object o)
@@ -44,9 +33,24 @@ public class PickSupport
         this.getPickableObjects().put(colorCode, new PickedObject(colorCode, o));
     }
 
-    public void addPickableObject(PickedObject po)
+    public void addPickableObject(int colorCode, Object o, Position position)
     {
-        this.getPickableObjects().put(po.getColorCode(), po);
+        this.getPickableObjects().put(colorCode, new PickedObject(colorCode, o, position, false));
+    }
+
+    public void addPickableObject(int colorCode, Object o, Position position, boolean isTerrain)
+    {
+        this.getPickableObjects().put(colorCode, new PickedObject(colorCode, o, position, isTerrain));
+    }
+
+    public void clearPickList()
+    {
+        this.getPickableObjects().clear();
+    }
+
+    protected Map<Integer, PickedObject> getPickableObjects()
+    {
+        return this.pickableObjects;
     }
 
     public PickedObject getTopObject(DrawContext dc, Point pickPoint)
@@ -79,24 +83,6 @@ public class PickSupport
         this.clearPickList();
 
         return pickedObject;
-    }
-
-    protected Map<Integer, PickedObject> getPickableObjects()
-    {
-        return this.pickableObjects;
-    }
-
-    /**
-     * Indicates whether two picked objects refer to the same user object.
-     *
-     * @param a the first picked object.
-     * @param b the second picked object.
-     *
-     * @return true if both objects are not null and they refer to the same user object, otherwise false.
-     */
-    public static boolean areSelectionsTheSame(PickedObject a, PickedObject b)
-    {
-        return a != null && b != null && a.getObject() == b.getObject();
     }
 }
 
