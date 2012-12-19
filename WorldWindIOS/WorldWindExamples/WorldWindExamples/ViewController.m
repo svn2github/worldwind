55ca2b20b82a8825b2dc56ae122a651d6934dbe7
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2012-12-19 03:28:57 +0000

    Modified ViewController to support device orientation changes on iOS 5.x.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1001 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/WorldWindExamples/WorldWindExamples/ViewController.m
+++ WorldWindIOS/WorldWindExamples/WorldWindExamples/ViewController.m
@@ -32,7 +32,7 @@
     self.view.opaque = YES;
 }
 
-- (void)viewDidLoad
+- (void) viewDidLoad
 {
     [super viewDidLoad];
 
@@ -42,6 +42,26 @@
 
     WWLayerList* layers = [[wwv sceneController] layers];
     [layers addLayer:[[WWBMNGOneImageLayer alloc] init]];
+}
+
+/*!
+    Returns YES if this view controller's contents should auto rotate in response to the specified orientation, and NO
+    otherwise. This returns YES for the iPad idom, and returns YES for the iPhone idom except when the specified
+    toInterfaceOrientation is UIInterfaceOrientationPortraitUpsideDown. This behavior matches the default supported
+    interface orientations in iOS 6.0.
+
+    This method is deprecated in iOS 6.0, but is required in iOS 5.x in order to support device orientation changes
+    other than portrait. In iOS 6.0, auto rotation and supported interface orientations are handled by entries in the
+    application's Info.plist file, or alternatively by overriding the method supportedInterfaceOrientations.
+
+    @result Returns YES.
+ */
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+        return YES;
+    else // UIUserInterfaceIdiomPhone
+        return toInterfaceOrientation != UIInterfaceOrientationPortraitUpsideDown;
 }
 
 @end
