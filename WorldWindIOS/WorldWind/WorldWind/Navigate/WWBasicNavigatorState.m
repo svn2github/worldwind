0a5451cb82a5ae6e1a01dcc04f8ce575ce3d6743
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-01-16 21:49:43 +0000

    Modified WWBasicNavigatorState's initializer to correctly handle the superclass initializer returning nil.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1089 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/WorldWind/WorldWind/Navigate/WWBasicNavigatorState.m
+++ WorldWindIOS/WorldWind/WorldWind/Navigate/WWBasicNavigatorState.m
@@ -27,21 +27,24 @@
 
     self = [super init];
 
-    _modelview = [[WWMatrix alloc] initWithMatrix:modelviewMatrix];
-    _projection = [[WWMatrix alloc] initWithMatrix:projectionMatrix];
-    _modelviewProjection = [[WWMatrix alloc] initWithMultiply:projectionMatrix matrixB:modelviewMatrix];
+    if (self != nil)
+    {
+        _modelview = [[WWMatrix alloc] initWithMatrix:modelviewMatrix];
+        _projection = [[WWMatrix alloc] initWithMatrix:projectionMatrix];
+        _modelviewProjection = [[WWMatrix alloc] initWithMultiply:projectionMatrix matrixB:modelviewMatrix];
 
-    // The eye point is computed by transforming the origin by the inverse of the modelview matrix.
-    WWMatrix* invModelview = [[WWMatrix alloc] initWithInverse:_modelview];
-    _eyePoint = [[WWVec4 alloc] initWithZeroVector];
-    [invModelview multiplyVector:_eyePoint];
+        // The eye point is computed by transforming the origin by the inverse of the modelview matrix.
+        WWMatrix* invModelview = [[WWMatrix alloc] initWithInverse:_modelview];
+        _eyePoint = [[WWVec4 alloc] initWithZeroVector];
+        [invModelview multiplyVector:_eyePoint];
 
-    _frustum = [projectionMatrix extractFrustum]; // returns normalized frustum plane vectors
+        _frustum = [projectionMatrix extractFrustum]; // returns normalized frustum plane vectors
 
-    // The model-coordinate frustum is computed by transforming the frustum by the transpose of the modelview matrix.
-    WWMatrix* modelviewTranspose = [[WWMatrix alloc] initWithTranspose:_modelview];
-    _frustumInModelCoordinates = [[WWFrustum alloc] initWithTransformedFrustum:_frustum matrix:modelviewTranspose];
-    // TODO: Should the MC frustum plane vectors be normalized after applying the transform?
+        // The model-coordinate frustum is computed by transforming the frustum by the transpose of the modelview matrix.
+        WWMatrix* modelviewTranspose = [[WWMatrix alloc] initWithTranspose:_modelview];
+        _frustumInModelCoordinates = [[WWFrustum alloc] initWithTransformedFrustum:_frustum matrix:modelviewTranspose];
+        // TODO: Should the MC frustum plane vectors be normalized after applying the transform?
+    }
 
     return self;
 }
