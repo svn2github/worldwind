8d38c387c030b130769794128ed2b7836ee12617
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-02-15 18:40:47 +0000

    Added data importer app.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1180 822214bb-dcf7-4c8d-a767-17ae0fb20d88

---
+++ WorldWind/src/gov/nasa/worldwindx/applications/dataimporter/FileSetMap.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2013 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+
+package gov.nasa.worldwindx.applications.dataimporter;
+
+import java.util.HashMap;
+
+/**
+ * Simply wraps a hash map of {@link FileSet}s.
+ *
+ * @author tag
+ * @version $Id$
+ */
+public class FileSetMap extends HashMap<Object, FileSet>
+{
+}
