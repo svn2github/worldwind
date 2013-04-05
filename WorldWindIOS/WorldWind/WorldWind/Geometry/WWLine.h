c4ab43a199cf5c117f68649f86d579a1dfe6d8cc
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-04-05 21:08:40 +0000

    Added an initial implementation of WWLine to the GeometryGroup.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1266 822214bb-dcf7-4c8d-a767-17ae0fb20d88

---
+++ WorldWindIOS/WorldWind/WorldWind/Geometry/WWLine.h
@@ -0,0 +1,77 @@
+/*
+ Copyright (C) 2013 United States Government as represented by the Administrator of the
+ National Aeronautics and Space Administration. All Rights Reserved.
+
+ @version $Id$
+ */
+
+#import <Foundation/Foundation.h>
+
+@class WWVec4;
+
+/**
+* Represents a line in model coordinates defined by an origin point and a unit-length direction vector.
+*
+* WWLine defines an infinite line that passes through a point in a given direction, and extends to infinity in both
+* directions. WWLine may be interpreted as a ray with a single endpoint extending to infinity along its direction, or as
+* a segment between two endpoints. Classes or methods that interpret WWLine as a ray or as a line segment are documented
+* as doing so.
+*
+* @warning WWLine instances are mutable.
+*/
+@interface WWLine : NSObject
+
+/// @name Line Attributes
+
+/// The point this passes through, in model coordinates.
+@property(nonatomic) WWVec4* origin;
+
+/// This line's unit-length direction vector, in model coordinates.
+@property(nonatomic) WWVec4* direction;
+
+/// @name Initializing Lines
+
+/**
+* Initializes a line with a specified origin point and unit-length direction vector.
+*
+* This method retains the specified origin and direction, and assigns them to its internal origin and direction
+* properties.
+*
+* @param origin The point the passes through, in model coordinates.
+* @param direction The line's direction vector, in model coordinates. This vector must have a length of one.
+*
+* @return The initialized line.
+*
+* @exception NSInvalidArgumentException If either parameter is nil, or if the direction has a length other than one.
+*/
+- (WWLine*) initWithOrigin:(WWVec4*)origin direction:(WWVec4*)direction;
+
+/// @name Operations on Lines
+
+/**
+* Computes a point on this line with a specified distance from the origin point.
+*
+* The distance may be any real value. Zero distance indicates the origin point, a negative distance indicates a point
+* on the line opposite the direction vector, and a positive distance indicates a point along the direction vector. The
+* computed point is stored in the result parameter after this method returns.
+*
+* @param distance The distance between the origin point and desired point on this line.
+* @param result A WWVec4 instance in which to return the point on this line.
+*
+* @exception NSInvalidArgumentException If the result is nil.
+*/
+- (void) pointAt:(double)distance result:(WWVec4*)result;
+
+/**
+* Computes the point on this line that is nearest to a specified point.
+*
+* The computed point is stored in the result parameter after this method returns.
+*
+* @param point The point by which the nearest point on this line is determined.
+* @param result A WWVec4 instance in which to return the nearest point.
+*
+* @exception NSInvalidArgumentException If either parameter is nil.
+*/
+- (void) nearestPointTo:(WWVec4*)point result:(WWVec4*)result;
+
+@end
---
+++ WorldWindIOS/WorldWind/WorldWind/Geometry/WWLine.m
@@ -0,0 +1,72 @@
+/*
+ Copyright (C) 2013 United States Government as represented by the Administrator of the
+ National Aeronautics and Space Administration. All Rights Reserved.
+
+ @version $Id$
+ */
+
+#import "WorldWind/Geometry/WWLine.h"
+#import "WorldWind/WWLog.h"
+#import "WWVec4.h"
+
+@implementation WWLine
+
+- (WWLine*) initWithOrigin:(WWVec4*)origin direction:(WWVec4*)direction
+{
+    if (origin == nil)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Origin is nil")
+    }
+
+    if (direction == nil)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Direction is nil")
+    }
+
+    if ([direction length3] != 1)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Direction has non-unit length")
+    }
+
+    self = [super init];
+
+    _origin = origin;
+    _direction = direction;
+
+    return self;
+}
+
+- (void) pointAt:(double)distance result:(WWVec4*)result
+{
+    if (result == nil)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Result is nil")
+    }
+
+    [WWVec4 pointOnLine:_origin direction:_direction t:distance result:result];
+}
+
+- (void) nearestPointTo:(WWVec4*)point result:(WWVec4*)result
+{
+    if (point == nil)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Point is nil")
+    }
+
+    if (result == nil)
+    {
+        WWLOG_AND_THROW(NSInvalidArgumentException, @"Result is nil")
+    }
+
+    // Compute a vector from this line's origin to the specified point.
+    [result set:point];
+    [result subtract3:_origin];
+
+    // Compute the projection of the vector onto this line's direction.
+    double proj = [result dot3:_direction] / [_direction dot3:_direction];
+
+    // Compute the point on this line corresponding to the vector's projection.
+    [self pointAt:proj result:result];
+}
+
+@end
