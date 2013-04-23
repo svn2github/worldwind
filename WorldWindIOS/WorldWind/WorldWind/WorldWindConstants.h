b7d28022f795aaa230b1bbbef444f201a2042a44
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-04-23 22:35:45 +0000

    Added constants used for extracting navigator parameters from a modelview matrix.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1295 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/WorldWind/WorldWind/WorldWindConstants.h
+++ WorldWindIOS/WorldWind/WorldWind/WorldWindConstants.h
@@ -15,19 +15,24 @@ extern NSString* const WW_GPU_PROGRAM;
 extern NSString* const WW_GPU_TEXTURE;
 extern NSString* const WW_GPU_VBO;
 extern NSString* const WW_GREAT_CIRCLE;
+extern NSString* const WW_HEADING;
 extern NSString* const WW_LINEAR;
 extern NSString* const WW_NAVIGATOR_ANIMATION_BEGAN;
 extern NSString* const WW_NAVIGATOR_ANIMATION_CANCELLED;
 extern NSString* const WW_NAVIGATOR_ANIMATION_ENDED;
 extern NSString* const WW_NAVIGATOR_GESTURE_RECOGNIZED;
+extern NSString* const WW_ORIGIN;
 extern NSString* const WW_REQUEST_REDRAW;
 extern NSString* const WW_RHUMB;
 extern NSString* const WW_SUCCEEDED;
 extern NSString* const WW_RETRIEVAL_STATUS;
 extern NSString* const WW_REQUEST_STATUS;
+extern NSString* const WW_RANGE;
+extern NSString* const WW_ROLL;
 extern NSString* const WW_TEXTURE_PVRTC_4BPP;
 extern NSString* const WW_TEXTURE_RGBA_5551;
 extern NSString* const WW_TEXTURE_RGBA_8888;
+extern NSString* const WW_TILT;
 extern NSString* const WW_URL;
 
 @interface WorldWindConstants : NSObject
--- WorldWindIOS/WorldWind/WorldWind/WorldWindConstants.m
+++ WorldWindIOS/WorldWind/WorldWind/WorldWindConstants.m
@@ -17,19 +17,24 @@ NSString* const WW_GPU_PROGRAM = @"gov.nasa.worldwind.GpuProgram";
 NSString* const WW_GPU_TEXTURE = @"gov.nasa.worldwind.GpuTexture";
 NSString* const WW_GPU_VBO = @"gov.nasa.worldwind.GpuVbo";
 NSString* const WW_GREAT_CIRCLE = @"gov.nasa.worldwind.GreatCircle";
+NSString* const WW_HEADING = @"gov.nasa.worldwind.Heading";
 NSString* const WW_LINEAR = @"gov.nasa.worldwind.Linear";
 NSString* const WW_NAVIGATOR_ANIMATION_BEGAN = @"gov.nasa.worldwind.NavigatorAnimationBegan";
 NSString* const WW_NAVIGATOR_ANIMATION_CANCELLED = @"gov.nasa.worldwind.NavigatorAnimationCancelled";
 NSString* const WW_NAVIGATOR_ANIMATION_ENDED = @"gov.nasa.worldwind.NavigatorAnimationEnded";
 NSString* const WW_NAVIGATOR_GESTURE_RECOGNIZED = @"gov.nasa.worldwind.NavigatorGestureRecognized";
+NSString* const WW_ORIGIN = @"gov.nasa.worldwind.Origin";
 NSString* const WW_REQUEST_REDRAW = @"gov.nasa.worldwind.RequestRedraw";
 NSString* const WW_RHUMB = @"gov.nasa.worldwind.Rhumb";
 NSString* const WW_SUCCEEDED = @"gov.nasa.worldwind.RetrievalSucceeded";
 NSString* const WW_RETRIEVAL_STATUS = @"gov.nasa.worldwind.RetrievalStatus";
 NSString* const WW_REQUEST_STATUS = @"gov.nasa.worldwind.RequestStatus";
+NSString* const WW_RANGE = @"gov.nasa.worldwind.Range";
+NSString* const WW_ROLL = @"gov.nasa.worldwind.Roll";
 NSString* const WW_TEXTURE_PVRTC_4BPP = @"gov.nasa.worldwind.TexturePVRTC4BPP";
 NSString* const WW_TEXTURE_RGBA_5551 = @"gov.nasa.worldwind.Texture5551";
 NSString* const WW_TEXTURE_RGBA_8888 = @"gov.nasa.worldwind.Texture8888";
+NSString* const WW_TILT = @"gov.nasa.worldwind.Tilt";
 NSString* const WW_URL = @"gov.nasa.worldwind.URL";
 
 @implementation WorldWindConstants
