3f11bc2c9f29136b9d27e3249af3cf15c1d98a89
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-06-07 16:30:37 +0000

    Added exception handling and some tests to prevent crashes when formint METAR data display.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1414 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/WorldWindExamples/WorldWindExamples/METARDataViewController.m
+++ WorldWindIOS/WorldWindExamples/WorldWindExamples/METARDataViewController.m
@@ -6,6 +6,7 @@
  */
 
 #import "METARDataViewController.h"
+#import "WWLog.h"
 
 
 @implementation METARDataViewController
@@ -27,7 +28,15 @@
 
 - (NSInteger) tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return section == 0 ? 11 : [[_entries objectForKey:@"sky_conditions"] count];
+    if (section == 0)
+    {
+        return 11;
+    }
+    else
+    {
+        NSArray* skyConditions = [_entries objectForKey:@"sky_conditions"];
+        return skyConditions != nil && [skyConditions count] > 0 ? [skyConditions count] : 0;
+    }
 }
 
 - (NSString*) tableView:(UITableView*)tableView titleForHeaderInSection:(NSInteger)section
@@ -37,108 +46,122 @@
 
 - (UITableViewCell*) tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-
     static NSString* cellIdentifier = @"cell";
 
-    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (cell == nil)
+    @try
     {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:cellIdentifier];
-    }
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (cell == nil)
+        {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:cellIdentifier];
+        }
 
-    if ([indexPath section] == 1)
-    {
-        NSArray* skyConditions = [_entries objectForKey:@"sky_conditions"];
-        [[cell textLabel] setText:nil];
-        [[cell detailTextLabel] setText:[skyConditions objectAtIndex:(NSUInteger)[indexPath row]]];
+        if ([indexPath section] == 1)
+        {
+            NSArray* skyConditions = [_entries objectForKey:@"sky_conditions"];
+            if (skyConditions != nil && [skyConditions count] > 0)
+            {
+                [[cell textLabel] setText:nil];
+                [[cell detailTextLabel] setText:[skyConditions objectAtIndex:(NSUInteger)[indexPath row]]];
+                return cell;
+            }
+            else
+            {
+                return nil;
+            }
+        }
+
+        switch ([indexPath row])
+        {
+            case 0:
+                [[cell textLabel] setText:@"Observed at"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"observation_time"]];
+                break;
+
+            case 1:
+                [[cell textLabel] setText:@"Station"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"station_id"]];
+                break;
+
+            case 2:
+            {
+                [[cell textLabel] setText:@"Temperature"];
+                NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"temp_c"]];
+                [detail appendString:@" C"];
+                [[cell detailTextLabel] setText:detail];
+                break;
+            }
+
+            case 3:
+            {
+                [[cell textLabel] setText:@"Dewpoint"];
+                NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"dewpoint_c"]];
+                [detail appendString:@" C"];
+                [[cell detailTextLabel] setText:detail];
+                break;
+            }
+
+            case 4:
+            {
+                [[cell textLabel] setText:@"Visibility"];
+                NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries
+                        objectForKey:@"visibility_statute_mi"]];
+                [detail appendString:@" st mi"];
+                [[cell detailTextLabel] setText:detail];
+                break;
+            }
+
+            case 5:
+            {
+                [[cell textLabel] setText:@"Altimeter"];
+                NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"altim_in_hg"]];
+                [detail appendString:@" Hg"];
+                [[cell detailTextLabel] setText:detail];
+                break;
+            }
+
+            case 6:
+            {
+                [[cell textLabel] setText:@"Elevation"];
+                NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"elevation_m"]];
+                [detail appendString:@" meters"];
+                [[cell detailTextLabel] setText:detail];
+                break;
+            }
+
+            case 7:
+                [[cell textLabel] setText:@"Latitude"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"latitude"]];
+                break;
+
+            case 8:
+                [[cell textLabel] setText:@"Longitude"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"longitude"]];
+                break;
+
+            case 9:
+                [[cell textLabel] setText:@"Category"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"flight_category"]];
+                break;
+
+            case 10:
+                [[cell textLabel] setText:@"Type"];
+                [[cell detailTextLabel] setText:[_entries objectForKey:@"metar_type"]];
+                break;
+
+            default:
+                [[cell textLabel] setText:@""];
+                [[cell detailTextLabel] setText:@""];
+                break;
+        }
+
         return cell;
     }
-
-    switch ([indexPath row])
+    @catch (NSException* exception)
     {
-        case 0:
-            [[cell textLabel] setText:@"Observed at"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"observation_time"]];
-            break;
-
-        case 1:
-            [[cell textLabel] setText:@"Station"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"station_id"]];
-            break;
-
-        case 2:
-        {
-            [[cell textLabel] setText:@"Temperature"];
-            NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"temp_c"]];
-            [detail appendString:@" C"];
-            [[cell detailTextLabel] setText:detail];
-            break;
-        }
-
-        case 3:
-        {
-            [[cell textLabel] setText:@"Dewpoint"];
-            NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"dewpoint_c"]];
-            [detail appendString:@" C"];
-            [[cell detailTextLabel] setText:detail];
-            break;
-        }
-
-        case 4:
-        {
-            [[cell textLabel] setText:@"Visibility"];
-            NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries
-                    objectForKey:@"visibility_statute_mi"]];
-            [detail appendString:@" st mi"];
-            [[cell detailTextLabel] setText:detail];
-            break;
-        }
-
-        case 5:
-        {
-            [[cell textLabel] setText:@"Altimeter"];
-            NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"altim_in_hg"]];
-            [detail appendString:@" Hg"];
-            [[cell detailTextLabel] setText:detail];
-            break;
-        }
-
-        case 6:
-        {
-            [[cell textLabel] setText:@"Elevation"];
-            NSMutableString* detail = [[NSMutableString alloc] initWithString:[_entries objectForKey:@"elevation_m"]];
-            [detail appendString:@" meters"];
-            [[cell detailTextLabel] setText:detail];
-            break;
-        }
-
-        case 7:
-            [[cell textLabel] setText:@"Latitude"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"latitude"]];
-            break;
-
-        case 8:
-            [[cell textLabel] setText:@"Longitude"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"longitude"]];
-            break;
-
-        case 9:
-            [[cell textLabel] setText:@"Category"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"flight_category"]];
-            break;
-
-        case 10:
-            [[cell textLabel] setText:@"Type"];
-            [[cell detailTextLabel] setText:[_entries objectForKey:@"metar_type"]];
-            break;
-
-        default:
-            [[cell textLabel] setText:@""];
-            [[cell detailTextLabel] setText:@""];
-            break;
+        WWLogE(@"Forming METAR data display", exception);
+        return nil;
     }
-
-    return cell;
 }
 
 @end
