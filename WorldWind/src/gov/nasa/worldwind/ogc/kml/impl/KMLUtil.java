714d5960875a90a3be480011c74e304d77abada0
pabercrombie <pabercrombie@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-08-17 18:00:09 +0000

    Added ability to rotate a KML GroundOverlay that is drawn with only a color, and not an image. Moved method that computes rotated corners from KMLSurfaceImageImpl to KMLUtil, since it is now used by both KMLSurfaceImageImpl and KMLSurfacePolygonImpl. See WWJ-245.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1551 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/ogc/kml/impl/KMLUtil.java
+++ WorldWind/src/gov/nasa/worldwind/ogc/kml/impl/KMLUtil.java
@@ -266,4 +266,57 @@ public class KMLUtil
 
         return new Position(latitude, longitude, height);
     }
+
+    /**
+     * Rotate the corners of a sector around a normal vector through the sector centroid.
+     *
+     * @param globe    Globe to use to compute rotated positions.
+     * @param sector   Sector to rotate.
+     * @param rotation Rotation angle. Positive angles produce counterclockwise rotation.
+     *
+     * @return List of rotated corners.
+     */
+    protected static java.util.List<LatLon> rotateSector(Globe globe, Sector sector, Angle rotation)
+    {
+        if (globe == null)
+        {
+            String message = Logging.getMessage("nullValue.GlobeIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+        if (sector == null)
+        {
+            String message = Logging.getMessage("nullValue.SectorIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+        if (rotation == null)
+        {
+            String message = Logging.getMessage("nullValue.AngleIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        LatLon[] corners = sector.getCorners();
+        java.util.List<LatLon> transformedCorners = new ArrayList<LatLon>(corners.length);
+
+        // Using the four corners of the sector to compute the rotation axis avoids problems with dateline
+        // spanning polygons.
+        Vec4[] verts = sector.computeCornerPoints(globe, 1);
+        Vec4 normalVec = verts[2].subtract3(verts[0]).cross3(verts[3].subtract3(verts[1])).normalize3();
+        Matrix rotationMatrix = Matrix.fromAxisAngle(rotation, normalVec);
+
+        Vec4 centerPoint = sector.computeCenterPoint(globe, 1);
+
+        // Rotate each point around the surface normal, and convert back to geographic
+        for (Vec4 point : verts)
+        {
+            point = point.subtract3(centerPoint).transformBy3(rotationMatrix).add3(centerPoint);
+            LatLon ll = globe.computePositionFromPoint(point);
+
+            transformedCorners.add(ll);
+        }
+
+        return transformedCorners;
+    }
 }
