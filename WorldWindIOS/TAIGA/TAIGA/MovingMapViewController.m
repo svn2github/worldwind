582012f7326c0021acb1bb9714b57e9ce31386e1
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-09-02 23:35:05 +0000

    Added elevation shading to TAIGA.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1573 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/TAIGA/TAIGA/MovingMapViewController.m
+++ WorldWindIOS/TAIGA/TAIGA/MovingMapViewController.m
@@ -14,6 +14,7 @@
 #import "FlightPathsLayer.h"
 #import "LayerListController.h"
 #import "AppConstants.h"
+#import "WWElevationShadingLayer.h"
 
 @implementation MovingMapViewController
 {
@@ -61,6 +62,9 @@
     WWLayerList* layers = [[_wwv sceneController] layers];
 
     WWLayer* layer = [[WWBMNGLandsatCombinedLayer alloc] init];
+    [layers addLayer:layer];
+
+    layer = [[WWElevationShadingLayer alloc] init];
     [layers addLayer:layer];
 
     flightPathsLayer = [[FlightPathsLayer alloc]
