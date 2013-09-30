202e2631a699301dfbd54e3033b7b295ac7d0273
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-09-30 02:12:33 +0000

    Updated version number.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1637 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/TAIGA/TAIGA/AppConstants.h
+++ WorldWindIOS/TAIGA/TAIGA/AppConstants.h
@@ -7,8 +7,8 @@
 
 #import <Foundation/Foundation.h>
 
-#define TAIGA_VERSION (@"0.0009")
-#define TAIGA_VERSION_DATE (@"9/25/13")
+#define TAIGA_VERSION (@"0.0010")
+#define TAIGA_VERSION_DATE (@"9/29/13")
 
 #define TAIGA_HIDDEN_LAYER (@"gov.nasa.worldwind.taiga.hiddenlayer")
 #define TAIGA_METERS_TO_FEET (3.28084)
