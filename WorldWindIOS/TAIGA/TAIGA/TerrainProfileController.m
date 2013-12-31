9d072356e5cbe23979162c56cdd2244b710542bc
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-12-31 02:04:50 +0000

    Synchronized the Terrain Altitude and Terrain Profile warning offsets.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1789 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/TAIGA/TAIGA/TerrainProfileController.m
+++ WorldWindIOS/TAIGA/TAIGA/TerrainProfileController.m
@@ -12,6 +12,7 @@
 #import "WWLocation.h"
 #import "WWPosition.h"
 #import "WWMath.h"
+#import "Settings.h"
 
 
 @implementation TerrainProfileController
@@ -35,18 +36,32 @@
     // Update the terrain profile.
     WWPosition* currentPosition = [[WWPosition alloc] initWithCLPosition:position];
     WWPosition* nextPosition = [[WWPosition alloc] init];
-    double angularDistance= DEGREES(5 * TAIGA_MILES_TO_METERS / TAIGA_EARTH_RADIUS);
+    double angularDistance = DEGREES(5 * TAIGA_MILES_TO_METERS / TAIGA_EARTH_RADIUS);
     [WWLocation greatCircleLocation:currentPosition azimuth:[position course] distance:angularDistance
                      outputLocation:nextPosition];
     NSArray* path = [[NSArray alloc] initWithObjects:currentPosition, nextPosition, nil];
     [_terrainProfileView setPath:path];
     [_terrainProfileView setAircraftAltitude:(float) currentPosition.altitude];
     [_terrainProfileView setMaxAltitude:(float) (1.2 * currentPosition.altitude)];
-    [_terrainProfileView setWarningAltitude:(float) ([currentPosition altitude] - 100)
+    float warningOffset = [Settings getFloatForName:TAIGA_SHADED_ELEVATION_OFFSET defaultValue:100];
+    [_terrainProfileView setWarningAltitude:(float) ([currentPosition altitude] - warningOffset)
                              dangerAltitude:(float) [currentPosition altitude]];
     [_terrainProfileView setLeftLabel:@"0 miles"];
     [_terrainProfileView setCenterLabel:@"2.5 miles"];
     [_terrainProfileView setRightLabel:@"5 miles"];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleSettingChange:)
+                                                 name:TAIGA_SETTING_CHANGED object:nil];
+}
+
+- (void) handleSettingChange:(NSNotification*)notification
+{
+    if ([[notification name] isEqualToString:TAIGA_SHADED_ELEVATION_OFFSET])
+    {
+        float warningOffset = [Settings getFloatForName:TAIGA_SHADED_ELEVATION_OFFSET defaultValue:100];
+        float currentAltitude = [_terrainProfileView dangerAltitude];
+        [_terrainProfileView setWarningAltitude:(currentAltitude - warningOffset) dangerAltitude:currentAltitude];
+    }
 }
 
 @end
