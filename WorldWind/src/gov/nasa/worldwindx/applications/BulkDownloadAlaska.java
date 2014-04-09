11204438caec67c0324d287dbe81be28127ef83e
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-04-09 23:14:09 +0000

    Changes to Alaska FAA sectionals bulk download.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@1911 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwindx/applications/BulkDownloadAlaska.java
+++ WorldWind/src/gov/nasa/worldwindx/applications/BulkDownloadAlaska.java
@@ -7,17 +7,19 @@
 package gov.nasa.worldwindx.applications;
 
 import gov.nasa.worldwind.BasicFactory;
-import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.*;
 import gov.nasa.worldwind.event.*;
 import gov.nasa.worldwind.geom.Sector;
 import gov.nasa.worldwind.retrieve.*;
 import gov.nasa.worldwind.terrain.CompoundElevationModel;
 
+import java.util.ArrayList;
+
 /**
  * @author tag
  * @version $Id$
- *
- * This class downloads specially configured imagery and elevations for the World Wind iOS TAIGA application.
+ *          <p/>
+ *          This class downloads specially configured imagery and elevations for the World Wind iOS TAIGA application.
  */
 public class BulkDownloadAlaska
 {
@@ -25,47 +27,74 @@ public class BulkDownloadAlaska
     {
         try
         {
-            Sector alaskaSector = Sector.fromDegrees(55.7, 71.1, -169.2, -129.5);
+            // Use three sectors to avoid capturing a lot of ocean area.
+            ArrayList<Sector> sectors = new ArrayList<Sector>(3);
+            sectors.add(Sector.fromDegrees(59, 71.4, -168.2, -141));
+            sectors.add(Sector.fromDegrees(51, 59, -180, -151.7));
+            sectors.add(Sector.fromDegrees(54.6, 60.5, -141, -130));
 
-            BulkRetrievable layer = (BulkRetrievable) BasicFactory.create(AVKey.LAYER_FACTORY,
-                "config/Earth/BMNG256.xml");
-            System.out.println(layer.getName());
-            BulkRetrievalThread thread = layer.makeLocal(alaskaSector, 0, new BulkRetrievalListener()
-            {
-                @Override
-                public void eventOccurred(BulkRetrievalEvent event)
-                {
-                    System.out.println(event.getItem());
-                }
-            });
-            thread.join();
+            BulkRetrievable layer;
+            BulkRetrievalThread thread;
+            AVListImpl params = new AVListImpl();
 
-            layer = (BulkRetrievable) BasicFactory.create(AVKey.LAYER_FACTORY,
-                "config/Earth/Landsat256.xml");
-            System.out.println(layer.getName());
-            thread = layer.makeLocal(alaskaSector, 0, new BulkRetrievalListener()
+            for (Sector sector : sectors)
             {
-                @Override
-                public void eventOccurred(BulkRetrievalEvent event)
+                layer = (BulkRetrievable) BasicFactory.create(AVKey.LAYER_FACTORY, "config/Earth/BMNG256.xml");
+                System.out.println(layer.getName());
+                thread = layer.makeLocal(sector, 0, new BulkRetrievalListener()
                 {
-                    System.out.println(event.getItem());
-                }
-            });
-            thread.join();
+                    @Override
+                    public void eventOccurred(BulkRetrievalEvent event)
+                    {
+                        System.out.println(event.getItem());
+                    }
+                });
+                thread.join();
 
-            CompoundElevationModel cem = (CompoundElevationModel) BasicFactory.create(AVKey.ELEVATION_MODEL_FACTORY,
-                "config/Earth/EarthElevations256.xml");
-            layer = (BulkRetrievable) cem.getElevationModels().get(0);
-            System.out.println(layer.getName());
-            thread = layer.makeLocal(alaskaSector, 0, new BulkRetrievalListener()
-            {
-                @Override
-                public void eventOccurred(BulkRetrievalEvent event)
+                params.setValue(AVKey.NUM_LEVELS, 9); // More than 9 levels is too large for TAIGA
+                layer = (BulkRetrievable) BasicFactory.create(AVKey.LAYER_FACTORY, "config/Earth/Landsat256.xml");
+                System.out.println(layer.getName());
+                thread = layer.makeLocal(sector, 0, new BulkRetrievalListener()
                 {
-                    System.out.println(event.getItem());
-                }
-            });
-            thread.join();
+                    @Override
+                    public void eventOccurred(BulkRetrievalEvent event)
+                    {
+                        System.out.println(event.getItem());
+                    }
+                });
+                thread.join();
+
+                params.setValue(AVKey.NUM_LEVELS, 8); // More than 8 levels is too large for TAIGA
+                layer = (BulkRetrievable) BasicFactory.create(AVKey.LAYER_FACTORY,
+                    "config/Earth/AlaskaFAASectionals.xml", params);
+                System.out.println(layer.getName());
+                thread = layer.makeLocal(sector, 0, new BulkRetrievalListener()
+                {
+                    @Override
+                    public void eventOccurred(BulkRetrievalEvent event)
+                    {
+                        System.out.println(event.getItem());
+                    }
+                });
+                thread.join();
+
+                params.setValue(AVKey.NUM_LEVELS, 9); // More than 9 levels is too large for TAIGA
+                CompoundElevationModel cem = (CompoundElevationModel) BasicFactory.create(AVKey.ELEVATION_MODEL_FACTORY,
+                    "config/Earth/EarthElevations256.xml");
+                layer = (BulkRetrievable) cem.getElevationModels().get(0);
+                System.out.println(layer.getName());
+                thread = layer.makeLocal(sector, 0, new BulkRetrievalListener()
+                {
+                    @Override
+                    public void eventOccurred(BulkRetrievalEvent event)
+                    {
+                        System.out.println(event.getItem());
+                    }
+                });
+                thread.join();
+            }
+
+
         }
         catch (InterruptedException e)
         {
