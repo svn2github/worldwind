fcf6fe4bb82c44c8727e1229f43a6103f8759051
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-07-09 15:20:27 +0000

    Added a simpler GlobeAnnotation example.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2131 822214bb-dcf7-4c8d-a767-17ae0fb20d88

---
+++ WorldWind/src/gov/nasa/worldwindx/examples/GlobeAnnotationExample.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+
+package gov.nasa.worldwindx.examples;
+
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.GlobeAnnotation;
+
+/**
+ * Tests a GlobeAnnotation near the dateline.
+ *
+ * @author tag
+ * @version $Id$
+ */
+public class GlobeAnnotationExample extends ApplicationTemplate
+{
+    protected static class AppFrame extends ApplicationTemplate.AppFrame
+    {
+
+        public AppFrame()
+        {
+            RenderableLayer layer = new RenderableLayer();
+            layer.setName("Annotation");
+            insertBeforeCompass(this.getWwd(), layer);
+
+            GlobeAnnotation ga = new GlobeAnnotation("AGL Annotation", Position.fromDegrees(20, 179.9, 1000));
+            layer.addRenderable(ga);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        ApplicationTemplate.start("World Wind Globe Annotation", AppFrame.class);
+    }
+}
