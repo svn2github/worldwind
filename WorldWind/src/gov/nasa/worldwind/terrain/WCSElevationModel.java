3f8704f70807197402d02093a2c0f3de37ead3ae
pabercrombie <pabercrombie@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-07-10 00:59:17 +0000

    Added restorable state support to WCSElevationModel.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2135 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/terrain/WCSElevationModel.java
+++ WorldWind/src/gov/nasa/worldwind/terrain/WCSElevationModel.java
@@ -34,6 +34,33 @@ public class WCSElevationModel extends BasicElevationModel
         super(wcsGetParamsFromCapsDoc(caps, params));
     }
 
+    /**
+     * Create a new elevation model from a serialized restorable state string.
+     *
+     * @param restorableStateInXml XML string in World Wind restorable state format.
+     *
+     * @see #getRestorableState()
+     */
+    public WCSElevationModel(String restorableStateInXml)
+    {
+        super(wcsRestorableStateToParams(restorableStateInXml));
+
+        RestorableSupport rs;
+        try
+        {
+            rs = RestorableSupport.parse(restorableStateInXml);
+        }
+        catch (Exception e)
+        {
+            // Parsing the document specified by stateInXml failed.
+            String message = Logging.getMessage("generic.ExceptionAttemptingToParseStateXml", restorableStateInXml);
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message, e);
+        }
+
+        this.doRestoreState(rs, null);
+    }
+
     protected static AVList wcsGetParamsFromDocument(Element domElement, AVList params)
     {
         if (domElement == null)
@@ -341,5 +368,83 @@ public class WCSElevationModel extends BasicElevationModel
         retriever.setConnectTimeout(10000);
         retriever.setReadTimeout(60000);
         retriever.call();
+    }
+
+    //**************************************************************//
+    //********************  Restorable Support  ********************//
+    //**************************************************************//
+
+    @Override
+    public void getRestorableStateForAVPair(String key, Object value,
+        RestorableSupport rs, RestorableSupport.StateObject context)
+    {
+        if (value instanceof URLBuilder)
+        {
+            rs.addStateValueAsString(context, AVKey.WCS_VERSION, ((URLBuilder) value).serviceVersion);
+        }
+        else if (value instanceof WCS100DescribeCoverage)
+        {
+            // Skip this entry. The DescribeCoverage parameters will already be present in the parameter list.
+        }
+        else
+        {
+            super.getRestorableStateForAVPair(key, value, rs, context);
+        }
+    }
+
+    protected static AVList wcsRestorableStateToParams(String stateInXml)
+    {
+        if (stateInXml == null)
+        {
+            String message = Logging.getMessage("nullValue.StringIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        RestorableSupport rs;
+        try
+        {
+            rs = RestorableSupport.parse(stateInXml);
+        }
+        catch (Exception e)
+        {
+            // Parsing the document specified by stateInXml failed.
+            String message = Logging.getMessage("generic.ExceptionAttemptingToParseStateXml", stateInXml);
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message, e);
+        }
+
+        AVList params = new AVListImpl();
+        wcsRestoreStateForParams(rs, null, params);
+        return params;
+    }
+
+    protected static void wcsRestoreStateForParams(RestorableSupport rs, RestorableSupport.StateObject context,
+        AVList params)
+    {
+        // Invoke the BasicElevationModel functionality.
+        restoreStateForParams(rs, null, params);
+
+        String s = rs.getStateValueAsString(context, AVKey.IMAGE_FORMAT);
+        if (s != null)
+            params.setValue(AVKey.IMAGE_FORMAT, s);
+
+        s = rs.getStateValueAsString(context, AVKey.TITLE);
+        if (s != null)
+            params.setValue(AVKey.TITLE, s);
+
+        s = rs.getStateValueAsString(context, AVKey.DISPLAY_NAME);
+        if (s != null)
+            params.setValue(AVKey.DISPLAY_NAME, s);
+
+        RestorableSupport.adjustTitleAndDisplayName(params);
+
+        s = rs.getStateValueAsString(context, AVKey.COVERAGE_IDENTIFIERS);
+        if (s != null)
+            params.setValue(AVKey.COVERAGE_IDENTIFIERS, s);
+
+        s = rs.getStateValueAsString(context, AVKey.WCS_VERSION);
+        if (s != null)
+            params.setValue(AVKey.TILE_URL_BUILDER, new URLBuilder(s, params));
     }
 }
--- WorldWind/test/gov/nasa/worldwind/ogc/wcs/WCSElevationModelCreationTest.java
+++ WorldWind/test/gov/nasa/worldwind/ogc/wcs/WCSElevationModelCreationTest.java
@@ -25,22 +25,9 @@ public class WCSElevationModelCreationTest
     {
         public void test001()
         {
-            WCS100Capabilities caps = new WCS100Capabilities("testData/WCS/WCSCapabilities003.xml");
-            WCS100DescribeCoverage coverage = new WCS100DescribeCoverage("testData/WCS/WCSDescribeCoverage001.xml");
-
-            try
-            {
-                caps.parse();
-                coverage.parse();
-            }
-            catch (XMLStreamException e)
-            {
-                e.printStackTrace();
-            }
-
-            AVList params = new AVListImpl();
-            params.setValue(AVKey.DOCUMENT, coverage);
-            WCSElevationModel elevationModel = new WCSElevationModel(caps, params);
+            WCSElevationModel elevationModel = this.createWCSElevationModel(
+                new WCS100Capabilities("testData/WCS/WCSCapabilities003.xml"),
+                new WCS100DescribeCoverage("testData/WCS/WCSDescribeCoverage001.xml"));
 
             assertEquals("Incorrect number of levels", 5, elevationModel.getLevels().getNumLevels());
             double bestResolution = elevationModel.getBestResolution(Sector.FULL_SPHERE) * 180.0 / Math.PI;
@@ -55,6 +42,60 @@ public class WCSElevationModelCreationTest
                 elevationModel.getLevels().getFirstLevel().getFormatSuffix());
             assertEquals("Incorrect format suffix", "worldwind26.arc.nasa.gov/_wms2/WW_NASA_SRTM30_900m_Tiled",
                 elevationModel.getLevels().getFirstLevel().getCacheName());
+        }
+
+        public void testRestoreState()
+        {
+            WCSElevationModel origElevationModel = this.createWCSElevationModel(
+                new WCS100Capabilities("testData/WCS/WCSCapabilities003.xml"),
+                new WCS100DescribeCoverage("testData/WCS/WCSDescribeCoverage001.xml"));
+
+            String restorableState = origElevationModel.getRestorableState();
+
+            WCSElevationModel newElevationModel = new WCSElevationModel(restorableState);
+
+            assertEquals("Incorrect number of levels",
+                origElevationModel.getLevels().getNumLevels(),
+                newElevationModel.getLevels().getNumLevels());
+
+            double epsilon = 0.0001;
+            double origBestResolution = origElevationModel.getBestResolution(Sector.FULL_SPHERE);
+            double newBestResolution = newElevationModel.getBestResolution(Sector.FULL_SPHERE);
+            assertTrue("Incorrect best resolution", Math.abs(origBestResolution - newBestResolution) < epsilon);
+
+            assertEquals("Min elevation incorrect",
+                origElevationModel.getMinElevation(),
+                newElevationModel.getMinElevation());
+            assertEquals("Max elevation incorrect",
+                origElevationModel.getMaxElevation(),
+                newElevationModel.getMaxElevation());
+
+            assertEquals("Incorrect dataset name",
+                origElevationModel.getLevels().getFirstLevel().getDataset(),
+                newElevationModel.getLevels().getFirstLevel().getDataset());
+            assertEquals("Incorrect format suffix",
+                origElevationModel.getLevels().getFirstLevel().getFormatSuffix(),
+                newElevationModel.getLevels().getFirstLevel().getFormatSuffix());
+            assertEquals("Incorrect cache name",
+                origElevationModel.getLevels().getFirstLevel().getCacheName(),
+                newElevationModel.getLevels().getFirstLevel().getCacheName());
+        }
+
+        private WCSElevationModel createWCSElevationModel(WCS100Capabilities caps, WCS100DescribeCoverage coverage)
+        {
+            try
+            {
+                caps.parse();
+                coverage.parse();
+            }
+            catch (XMLStreamException e)
+            {
+                e.printStackTrace();
+            }
+
+            AVList params = new AVListImpl();
+            params.setValue(AVKey.DOCUMENT, coverage);
+            return new WCSElevationModel(caps, params);
         }
     }
 
