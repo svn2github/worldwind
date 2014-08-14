6f7ee2031a345390c1bf491231e7af8ef3ebfbdf
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-08-14 18:14:15 +0000

    Added latitude limits field to ProjectionMercator. MDA-42.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2229 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/globes/projections/ProjectionMercator.java
+++ WorldWind/src/gov/nasa/worldwind/globes/projections/ProjectionMercator.java
@@ -8,6 +8,7 @@ package gov.nasa.worldwind.globes.projections;
 
 import gov.nasa.worldwind.geom.*;
 import gov.nasa.worldwind.globes.*;
+import gov.nasa.worldwind.util.Logging;
 
 /**
  * @author tag
@@ -15,6 +16,8 @@ import gov.nasa.worldwind.globes.*;
  */
 public class ProjectionMercator implements GeographicProjection
 {
+    protected double[] limits = new double[] {-78, 78};
+
     @Override
     public String getName()
     {
@@ -27,13 +30,59 @@ public class ProjectionMercator implements GeographicProjection
         return true;
     }
 
+    /**
+     * Indicates the latitude limits for this projection.
+     *
+     * @return The latitude limits in degrees for this projection. Position 0 indicates the lower limit, position 1 the
+     * upper limit.
+     */
+    public double[] getLimits()
+    {
+        return limits;
+    }
+
+    /**
+     * Specifies the latitude limits for this projection.
+     *
+     * @param limits The latitude limits, in degrees. Position 0 indicates the lower limit, e.g., -78, position 1
+     *               indicates the upper limit, e.g., 78.
+     *
+     * @throws java.lang.IllegalArgumentException if the specified limits array is null or the limits are outside the
+     *                                            normal range of latitude, +/-90 degrees.
+     */
+    public void setLimits(double[] limits)
+    {
+        if (limits == null)
+        {
+            String message = Logging.getMessage("nullValue.ArrayIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (limits[0] < -90 || limits[0] > 90)
+        {
+            String message = Logging.getMessage("generic.LatitudeOutOfRange", limits[0]);
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (limits[1] < -90 || limits[1] > 90)
+        {
+            String message = Logging.getMessage("generic.LatitudeOutOfRange", limits[1]);
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        this.limits = limits;
+    }
+
     @Override
     public Vec4 geographicToCartesian(Globe globe, Angle latitude, Angle longitude, double metersElevation, Vec4 offset)
     {
-        if (latitude.degrees > 75)
-            latitude = Angle.fromDegrees(75);
-        if (latitude.degrees < -75)
-            latitude = Angle.fromDegrees(-75);
+        if (latitude.degrees > this.limits[1])
+            latitude = Angle.fromDegrees(this.limits[1]);
+        if (latitude.degrees < this.limits[0])
+            latitude = Angle.fromDegrees(this.limits[0]);
 
         double xOffset = offset != null ? offset.x : 0;
 
