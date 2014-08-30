70d9f6e012cc267e46bd9f6fa6470a524692c491
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-08-30 17:00:35 +0000

    App update.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2287 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWindIOS/TAIGA/TAIGA/AppConstants.h
+++ WorldWindIOS/TAIGA/TAIGA/AppConstants.h
@@ -10,8 +10,8 @@
 
 static const int ddLogLevel = LOG_LEVEL_INFO;
 
-#define TAIGA_VERSION (@"0.0043")
-#define TAIGA_VERSION_DATE (@"8/18/14")
+#define TAIGA_VERSION (@"0.0044")
+#define TAIGA_VERSION_DATE (@"8/25/14")
 
 #define TAIGA_DATA_HOST @"worldwindserver.net"
 
