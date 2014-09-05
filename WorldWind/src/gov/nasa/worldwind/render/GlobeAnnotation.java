5b3ff9fffd36bb757f37aea551d7823f03357fc0
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-09-05 17:05:39 +0000

    Fixed MDA-60, GlobeAnnotations still have altitude in 2D/Flat mode.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2297 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/render/GlobeAnnotation.java
+++ WorldWind/src/gov/nasa/worldwind/render/GlobeAnnotation.java
@@ -394,11 +394,15 @@ public class GlobeAnnotation extends AbstractAnnotation implements Locatable, Mo
         Position pos = this.getPosition();
         Integer altitudeMode = this.getAltitudeMode();
 
-        if (altitudeMode == null)
+        if (dc.is2DGlobe())
+        {
+            drawPoint = dc.computeTerrainPoint(pos.getLatitude(), pos.getLongitude(), 0);
+        }
+        else if (altitudeMode == null)
         {
             drawPoint = getAnnotationDrawPointLegacy(dc);
         }
-        else if (altitudeMode == WorldWind.CLAMP_TO_GROUND || dc.is2DGlobe())
+        else if (altitudeMode == WorldWind.CLAMP_TO_GROUND)
         {
             drawPoint = dc.computeTerrainPoint(pos.getLatitude(), pos.getLongitude(), 0);
         }
