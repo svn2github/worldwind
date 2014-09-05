e6b6a539c65f22906edb9522b85322776c2a3745
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-09-05 22:11:54 +0000

    Added annotation to SurfaceShapeEditor.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2300 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwind/util/SurfaceShapeEditor.java
+++ WorldWind/src/gov/nasa/worldwind/util/SurfaceShapeEditor.java
@@ -7,6 +7,7 @@
 package gov.nasa.worldwind.util;
 
 import gov.nasa.worldwind.*;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.event.*;
 import gov.nasa.worldwind.geom.*;
 import gov.nasa.worldwind.globes.Globe;
@@ -32,16 +33,21 @@ public class SurfaceShapeEditor implements SelectListener
     protected final WorldWindow wwd;
     protected SurfaceShape shape;
     protected MarkerLayer controlPointLayer;
-    protected RenderableLayer rotationLineLayer;
+    protected RenderableLayer accessoryLayer;
+    protected RenderableLayer annotationLayer;
+    protected EditorAnnotation annotation;
     protected boolean armed;
 
     protected boolean active;
     protected int activeOperation = NONE;
     protected Position previousPosition = null;
+    protected ControlPointMarker currentSizingMarker;
 
     protected static class ControlPointMarker extends BasicMarker
     {
         protected int index;
+        protected Double size;
+        protected Angle rotation;
 
         public ControlPointMarker(Position position, MarkerAttributes attrs, int index)
         {
@@ -78,8 +84,8 @@ public class SurfaceShapeEditor implements SelectListener
         this.controlPointLayer.setElevation(0);
         this.controlPointLayer.setKeepSeparated(false);
 
-        this.rotationLineLayer = new RenderableLayer();
-        this.rotationLineLayer.setPickEnabled(false);
+        this.accessoryLayer = new RenderableLayer();
+        this.accessoryLayer.setPickEnabled(false);
 
         ShapeAttributes lineAttrs = new BasicShapeAttributes();
         lineAttrs.setOutlineMaterial(Material.GREEN);
@@ -88,7 +94,13 @@ public class SurfaceShapeEditor implements SelectListener
         lineLocations.add(LatLon.ZERO);
         lineLocations.add(LatLon.ZERO);
         SurfacePolyline rotationLine = new SurfacePolyline(lineAttrs, lineLocations);
-        this.rotationLineLayer.addRenderable(rotationLine);
+        this.accessoryLayer.addRenderable(rotationLine);
+
+        this.annotationLayer = new RenderableLayer();
+        this.annotationLayer.setPickEnabled(false);
+
+        this.annotation = new EditorAnnotation("");
+        this.annotationLayer.addRenderable(this.annotation);
     }
 
     public WorldWindow getWwd()
@@ -130,11 +142,14 @@ public class SurfaceShapeEditor implements SelectListener
         if (!this.controlPointLayer.isEnabled())
             this.controlPointLayer.setEnabled(true);
 
-        if (!layers.contains(this.rotationLineLayer))
-            layers.add(this.rotationLineLayer);
+        if (!layers.contains(this.accessoryLayer))
+            layers.add(this.accessoryLayer);
 
-        if (!this.rotationLineLayer.isEnabled())
-            this.rotationLineLayer.setEnabled(true);
+        if (!this.accessoryLayer.isEnabled())
+            this.accessoryLayer.setEnabled(true);
+
+        if (!layers.contains(this.annotationLayer))
+            layers.add(this.annotationLayer);
 
         this.updateControlPoints();
 
@@ -146,7 +161,8 @@ public class SurfaceShapeEditor implements SelectListener
         LayerList layers = this.wwd.getModel().getLayers();
 
         layers.remove(this.controlPointLayer);
-        layers.remove(this.rotationLineLayer);
+        layers.remove(this.accessoryLayer);
+        layers.remove(this.annotationLayer);
 
         wwd.removeSelectListener(this);
 
@@ -168,6 +184,7 @@ public class SurfaceShapeEditor implements SelectListener
             this.activeOperation = NONE;
             this.previousPosition = null;
             ((Component) this.wwd).setCursor(null);
+            this.updateAnnotation(null);
         }
         else if (event.getEventAction().equals(SelectEvent.ROLLOVER))
         {
@@ -179,12 +196,23 @@ public class SurfaceShapeEditor implements SelectListener
                 cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
             else if (this.activeOperation == SIZING)
                 cursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
-            else if (event.getTopObject() != null && event.getTopObject() instanceof SurfaceShape)
+            else if (event.getTopObject() != null && event.getTopObject() == this.shape)
                 cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
             else if (event.getTopObject() != null && event.getTopObject() instanceof Marker)
                 cursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
 
             ((Component) this.wwd).setCursor(cursor);
+
+            if (this.activeOperation == MOVING)
+                this.updateShapeAnnotation((SurfaceShape) event.getTopObject());
+            else if (this.activeOperation == SIZING)
+                this.updateAnnotation(this.currentSizingMarker);
+            else if (event.getTopObject() != null && event.getTopObject() == this.shape)
+                this.updateShapeAnnotation((SurfaceShape) event.getTopObject());
+            else if (event.getTopObject() != null && event.getTopObject() instanceof ControlPointMarker)
+                this.updateAnnotation((ControlPointMarker) event.getTopObject());
+            else
+                this.updateAnnotation(null);
         }
         else if (event.getEventAction().equals(SelectEvent.LEFT_PRESS))
         {
@@ -208,6 +236,7 @@ public class SurfaceShapeEditor implements SelectListener
                 this.activeOperation = MOVING;
                 this.dragWholeShape(dragEvent, topObject);
                 this.updateControlPoints();
+                this.updateShapeAnnotation((SurfaceShape) topObject);
                 event.consume();
             }
             else if (dragEvent.getTopPickedObject().getParentLayer() == this.controlPointLayer
@@ -265,6 +294,8 @@ public class SurfaceShapeEditor implements SelectListener
     {
         if (!(topObject instanceof ControlPointMarker))
             return;
+
+        this.currentSizingMarker = (ControlPointMarker) topObject;
 
         // If the terrain beneath the control point is null, then the user is attempting to drag the handle off the
         // globe. This is not a valid state for SurfaceImage, so we will ignore this action but keep the drag operation
@@ -451,6 +482,8 @@ public class SurfaceShapeEditor implements SelectListener
             double deltaAngle = this.computeHeadingDelta(centerPoint, previousPoint, terrainPoint, delta);
             ellipse.setHeading(Angle.fromRadians(ellipse.getHeading().getRadians() + deltaAngle));
         }
+
+        this.updateAnnotation(controlPoint);
     }
 
     protected void updateSurfacePolygonControlPoints()
@@ -513,6 +546,9 @@ public class SurfaceShapeEditor implements SelectListener
         {
             markers.iterator().next().setPosition(new Position(cpPosition, 0));
         }
+
+        Iterator<Marker> markerIterator = this.controlPointLayer.getMarkers().iterator();
+        ((ControlPointMarker) markerIterator.next()).size = circle.getRadius();
     }
 
     protected void updateSurfaceSquareControlPoints()
@@ -549,6 +585,10 @@ public class SurfaceShapeEditor implements SelectListener
             markerIterator.next().setPosition(new Position(cpPosition, 0));
             markerIterator.next().setPosition(new Position(cpPositionR, 0));
         }
+
+        Iterator<Marker> markerIterator = this.controlPointLayer.getMarkers().iterator();
+        ((ControlPointMarker) markerIterator.next()).size = square.getSize();
+        ((ControlPointMarker) markerIterator.next()).rotation = square.getHeading();
 
         this.updateRotationLine(square.getCenter(), cpPositionR);
     }
@@ -593,6 +633,11 @@ public class SurfaceShapeEditor implements SelectListener
             markerIterator.next().setPosition(new Position(cpPositionH, 0));
             markerIterator.next().setPosition(new Position(cpPositionR, 0));
         }
+
+        Iterator<Marker> markerIterator = this.controlPointLayer.getMarkers().iterator();
+        ((ControlPointMarker) markerIterator.next()).size = quad.getWidth();
+        ((ControlPointMarker) markerIterator.next()).size = quad.getHeight();
+        ((ControlPointMarker) markerIterator.next()).rotation = quad.getHeading();
 
         this.updateRotationLine(quad.getCenter(), cpPositionR);
     }
@@ -640,6 +685,11 @@ public class SurfaceShapeEditor implements SelectListener
             markerIterator.next().setPosition(new Position(cpPositionR, 0));
         }
 
+        Iterator<Marker> markerIterator = this.controlPointLayer.getMarkers().iterator();
+        ((ControlPointMarker) markerIterator.next()).size = ellipse.getMajorRadius();
+        ((ControlPointMarker) markerIterator.next()).size = ellipse.getMinorRadius();
+        ((ControlPointMarker) markerIterator.next()).rotation = ellipse.getHeading();
+
         this.updateRotationLine(ellipse.getCenter(), cpPositionR);
     }
 
@@ -648,8 +698,51 @@ public class SurfaceShapeEditor implements SelectListener
         java.util.List<LatLon> lineLocations = new ArrayList<LatLon>(2);
         lineLocations.add(centerPosition);
         lineLocations.add(cpPositionR);
-        SurfacePolyline rotationLine = (SurfacePolyline) this.rotationLineLayer.getRenderables().iterator().next();
+        SurfacePolyline rotationLine = (SurfacePolyline) this.accessoryLayer.getRenderables().iterator().next();
         rotationLine.setLocations(lineLocations);
+    }
+
+    protected void updateShapeAnnotation(SurfaceShape shape)
+    {
+        LatLon center = null;
+        if (shape instanceof SurfaceEllipse)
+            center = ((SurfaceEllipse) shape).getCenter();
+        else if (shape instanceof SurfaceQuad)
+            center = ((SurfaceQuad) shape).getCenter();
+
+        if (center != null)
+        {
+            ControlPointMarker dummyMarker = new ControlPointMarker(new Position(center, 0),
+                new BasicMarkerAttributes(), 0);
+            this.updateAnnotation(dummyMarker);
+        }
+        else
+        {
+            this.updateAnnotation(null);
+        }
+    }
+
+    protected void updateAnnotation(ControlPointMarker marker)
+    {
+        if (marker == null)
+        {
+            this.annotationLayer.setEnabled(false);
+            return;
+        }
+
+        this.annotationLayer.setEnabled(true);
+
+        Vec4 mcPoint = wwd.getModel().getGlobe().computePointFromLocation(marker.getPosition());
+        Vec4 screenPoint = wwd.getView().project(mcPoint);
+        this.annotation.setScreenPoint(new Point((int) screenPoint.x, (int) screenPoint.y));
+
+        String annotationText = marker.getPosition().toString();
+        if (marker.size != null)
+            annotationText = marker.size.toString();
+        else if (marker.rotation != null)
+            annotationText = marker.rotation.toString();
+
+        this.annotation.setText(annotationText);
     }
 
     protected double computeHeadingDelta(Vec4 centerPoint, Vec4 previousPoint, Vec4 terrainPoint, Vec4 delta)
@@ -660,5 +753,75 @@ public class SurfaceShapeEditor implements SelectListener
         double sign = cross.z >= 0 ? -1 : 1;
 
         return sign * Math.atan2(delta.getLength3(), vP.getLength3());
+    }
+
+    public class EditorAnnotation extends ScreenAnnotation
+    {
+        private Point tooltipOffset = new Point(5, 5);
+
+        /**
+         * Create a tool tip using specified text.
+         *
+         * @param text the text to display in the tool tip.
+         */
+        public EditorAnnotation(String text)
+        {
+            super(text, new Point(0, 0)); // (0,0) is a dummy; the actual point is determined when rendering
+
+            this.initializeAttributes();
+        }
+
+        protected void initializeAttributes()
+        {
+            this.attributes.setAdjustWidthToText(AVKey.SIZE_FIT_TEXT);
+            this.attributes.setFrameShape(AVKey.SHAPE_RECTANGLE);
+            this.attributes.setTextColor(Color.BLACK);
+            this.attributes.setBackgroundColor(new Color(1f, 1f, 1f, 0.8f));
+            this.attributes.setCornerRadius(5);
+            this.attributes.setBorderColor(new Color(0xababab));
+            this.attributes.setFont(Font.decode("Arial-PLAIN-12"));
+            this.attributes.setTextAlign(AVKey.CENTER);
+            this.attributes.setInsets(new Insets(5, 5, 5, 5));
+        }
+
+        protected int getOffsetX()
+        {
+            return this.tooltipOffset != null ? this.tooltipOffset.x : 0;
+        }
+
+        protected int getOffsetY()
+        {
+            return this.tooltipOffset != null ? this.tooltipOffset.y : 0;
+        }
+
+        @Override
+        protected void doRenderNow(DrawContext dc)
+        {
+            this.getAttributes().setDrawOffset(
+                new Point(this.getBounds(dc).width / 2 + this.getOffsetX(), this.getOffsetY()));
+            this.setScreenPoint(this.adjustDrawPointToViewport(this.getScreenPoint(), this.getBounds(dc),
+                dc.getView().getViewport()));
+
+            super.doRenderNow(dc);
+        }
+
+        protected Point adjustDrawPointToViewport(Point point, Rectangle bounds, Rectangle viewport) // TODO
+        {
+            return point;
+//            int x = point.x;
+//            int y = (int) viewport.getHeight() - point.y - 1;
+//
+//            if (x + this.getOffsetX() + bounds.getWidth() > viewport.getWidth())
+//                x = (int) (viewport.getWidth() - bounds.getWidth()) - 1 - this.getOffsetX();
+//            else if (x < 0)
+//                x = 0;
+//
+//            if (y + this.getOffsetY() + bounds.getHeight() > viewport.getHeight())
+//                y = (int) (viewport.getHeight() - bounds.getHeight()) - 1 - this.getOffsetY();
+//            else if (y < 0)
+//                y = bounds.height;
+//
+//            return new java.awt.Point(x, y);
+        }
     }
 }
