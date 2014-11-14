fb669a84d638d1d74e890bc37c654ae2e1efd407
dcollins <dcollins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-11-14 17:52:34 +0000

    Renamed the ContourBuilding example as ContourBuilderExample and added documentation.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2429 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WorldWind/src/gov/nasa/worldwindx/examples/ContourBuilderExample.java
+++ WorldWind/src/gov/nasa/worldwindx/examples/ContourBuilderExample.java
@@ -20,46 +20,65 @@ import java.util.*;
 import java.util.List;
 
 /**
+ * Shows how to use the {@link gov.nasa.worldwind.util.ContourBuilder} class to compute contour lines in an arbitrary
+ * rectangular array of numeric values.
+ * <p/>
+ * This example creates a 60x60 rectangular array of floating point values in the range from 0.0 to 1.0, inclusive. The
+ * array is displayed on the globe as an AnalyticSurface by mapping each array value to a color, where an array value of
+ * 0.0 maps to hue 0, an array value of 1.0 maps to hue 360, and values in between are interpolated between those two
+ * hues. Contour lines are created at the boundaries between adjacent colors using ContourBuilder, and are displayed on
+ * the globe as Path shapes.
+ *
  * @author dcollins
  * @version $Id$
  */
-public class ContourBuilding extends ApplicationTemplate
+public class ContourBuilderExample extends ApplicationTemplate
 {
     public static class AppFrame extends ApplicationTemplate.AppFrame
     {
-        protected Sector gridSector;
-        protected int gridWidth;
-        protected int gridHeight;
-        protected double[] gridValues;
+        protected Sector arraySector;
+        protected int arrayWidth;
+        protected int arrayHeight;
+        protected double[] arrayValues;
 
         public AppFrame()
         {
-            this.createGrid();
+            // Create the rectangular array of numeric values in the range from 0.0 to 1.0, inclusive, and assign the
+            // array a geographic sector.
+            this.createRectangularArray();
 
-            RenderableLayer fieldLayer = new RenderableLayer();
-            fieldLayer.setName("Grid Values");
-            this.getWwd().getModel().getLayers().add(fieldLayer);
-            this.addGridShapes(fieldLayer);
+            // Display the rectangular array of numeric values as a surface that maps each numeric value to a hue, and
+            // interpolates the colors between array points.
+            RenderableLayer arrayLayer = new RenderableLayer();
+            arrayLayer.setName("Rectangular Array");
+            this.getWwd().getModel().getLayers().add(arrayLayer);
+            this.addRectangularArrayShapes(arrayLayer);
 
+            // Create a layer to group the contour line shapes.
             RenderableLayer contourLayer = new RenderableLayer();
-            contourLayer.setName("Contours");
+            contourLayer.setName("Contour Lines");
             this.getWwd().getModel().getLayers().add(contourLayer);
 
-            ContourBuilder cb = new ContourBuilder(this.gridWidth, this.gridHeight, this.gridValues);
+            // Create a ContourBuilder with the rectangular array of numeric values as a one-dimensional array of
+            // floating point numbers. The contour builder assumes that the array is organized in row-major order, with
+            // the first value indicating the value at the upper-left corner.
+            ContourBuilder cb = new ContourBuilder(this.arrayWidth, this.arrayHeight, this.arrayValues);
 
+            // Build contour lines for a list of pre-determined threshold values. Contour line coordinates are computed
+            // by mapping the rectangular array's coordinates to a geographic sector.
             for (double value : Arrays.asList(0.083, 0.250, 0.416, 0.583, 0.75, 0.916))
             {
-                List<List<Position>> contourList = cb.buildContourLines(value, this.gridSector, 0);
+                List<List<Position>> contourList = cb.buildContourLines(value, this.arraySector, 0); // altitude 0
                 this.addContourShapes(contourList, value, contourLayer);
             }
         }
 
-        protected void createGrid()
+        protected void createRectangularArray()
         {
-            this.gridSector = Sector.fromDegrees(20, 30, -110, -100);
-            this.gridWidth = 60;
-            this.gridHeight = 60;
-            this.gridValues = ExampleUtil.readCommaDelimitedNumbers(
+            this.arraySector = Sector.fromDegrees(20, 30, -110, -100);
+            this.arrayWidth = 60;
+            this.arrayHeight = 60;
+            this.arrayValues = ExampleUtil.readCommaDelimitedNumbers(
                 "gov/nasa/worldwindx/examples/data/GridValues01_60x60.csv");
         }
 
@@ -83,11 +102,11 @@ public class ContourBuilding extends ApplicationTemplate
             }
         }
 
-        protected void addGridShapes(RenderableLayer layer)
+        protected void addRectangularArrayShapes(RenderableLayer layer)
         {
             ArrayList<AnalyticSurface.GridPointAttributes> pointAttrs =
                 new ArrayList<AnalyticSurface.GridPointAttributes>();
-            for (double value : this.gridValues)
+            for (double value : this.arrayValues)
             {
                 Color color = this.colorForValue(value, 0.5); // color for value at 50% brightness
                 pointAttrs.add(AnalyticSurface.createGridPointAttributes(value, color));
@@ -99,8 +118,8 @@ public class ContourBuilding extends ApplicationTemplate
 
             AnalyticSurface surface = new AnalyticSurface();
             surface.setSurfaceAttributes(attrs);
-            surface.setSector(this.gridSector);
-            surface.setDimensions(this.gridWidth, this.gridHeight);
+            surface.setSector(this.arraySector);
+            surface.setDimensions(this.arrayWidth, this.arrayHeight);
             surface.setValues(pointAttrs);
             surface.setAltitudeMode(WorldWind.CLAMP_TO_GROUND);
             layer.addRenderable(surface);
@@ -108,12 +127,12 @@ public class ContourBuilding extends ApplicationTemplate
 
         protected Color colorForValue(double value, double brightness)
         {
-            return Color.getHSBColor((float) value, 1.0f, (float) brightness); // use field value as hue
+            return Color.getHSBColor((float) value, 1.0f, (float) brightness); // use array value as hue
         }
 
         protected String textForValue(double value)
         {
-            return String.format("%.0f%%", 100 * value); // use field value as percentage
+            return String.format("%.2f", value);
         }
     }
 
