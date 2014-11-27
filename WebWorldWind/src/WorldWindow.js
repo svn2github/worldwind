3a22884d4b69d27554157f118edbf47904d52517
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-11-27 19:40:02 +0000

    Formatting changes in WorldWindow.js.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2480 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WebWorldWind/src/WorldWindow.js
+++ WebWorldWind/src/WorldWindow.js
@@ -6,46 +6,46 @@
  * @exports WorldWindow
  * @version $Id$
  */
-define(['src/util/Logger'], function (Logger) {
-    "use strict";
+define([
+        'src/util/Logger'],
+    function (Logger) {
+        "use strict";
 
-    /**
-     * Constructs a World Wind window for an HTML canvas.
-     * @alias WorldWindow
-     * @constructor
-     * @classdesc Represents a World Wind window for an HTML canvas.
-     * @param canvasName the name assigned to the canvas in the HTML page.
-     */
-    var WorldWindow = function (canvasName) {
-        this.canvas = document.getElementById(canvasName);
+        /**
+         * Constructs a World Wind window for an HTML canvas.
+         * @alias WorldWindow
+         * @constructor
+         * @classdesc Represents a World Wind window for an HTML canvas.
+         * @param canvasName the name assigned to the canvas in the HTML page.
+         */
+        var WorldWindow = function (canvasName) {
+            this.canvas = document.getElementById(canvasName);
 
-        this.canvas.addEventListener("webglcontextlost", handleContextLost, false);
-        this.canvas.addEventListener("webglcontextrestored", handleContextRestored, false);
+            this.canvas.addEventListener("webglcontextlost", handleContextLost, false);
+            this.canvas.addEventListener("webglcontextrestored", handleContextRestored, false);
 
-        function handleContextLost(event)
-        {
-            event.preventDefault();
-        }
+            function handleContextLost(event) {
+                event.preventDefault();
+            }
 
-        function handleContextRestored(event)
-        {
-        }
-    };
+            function handleContextRestored(event) {
+            }
+        };
 
-    /**
-     * Redraws the window.
-     */
-    WorldWindow.prototype.render = function () {
-        if (!window.WebGLRenderingContext) {
-            Logger.log(Logger.LEVEL_SEVERE, "Canvas does not support WebGL");
-            return;
-        }
+        /**
+         * Redraws the window.
+         */
+        WorldWindow.prototype.render = function () {
+            if (!window.WebGLRenderingContext) {
+                Logger.log(Logger.LEVEL_SEVERE, "Canvas does not support WebGL");
+                return;
+            }
 
-        var gl = this.canvas.getContext("webgl");
+            var gl = this.canvas.getContext("webgl");
 
-        gl.clearColor(1.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-    };
+            gl.clearColor(1.0, 0.0, 0.0, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+        };
 
-    return WorldWindow;
-});
+        return WorldWindow;
+    });
