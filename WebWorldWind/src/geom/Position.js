0869c702b05464a79592eade045e82139208ceac
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-11-28 23:43:52 +0000

    Completed implementation of Position.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2488 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WebWorldWind/src/geom/Position.js
+++ WebWorldWind/src/geom/Position.js
@@ -24,7 +24,7 @@ define([
          * Constructs a position from a specified latitude and longitude in degrees and altitude in meters.
          * @alias Position
          * @constructor
-         * @classdesc Represents a latitude, longitude pair.
+         * @classdesc Represents a latitude, longitude, altitude triple.
          * @param {Number} latitude The latitude in degrees.
          * @param {Number} longitude The longitude in degrees.
          * @param {Number} altitude The altitude in meters.
@@ -69,28 +69,32 @@ define([
         };
 
         /**
-         * Creates a new position from a specified position.
+         * Sets this position to the latitude and longitude of a specified position.
          * @param {Position} position The position to copy.
-         * @returns {Position} The new position, initialized to the values of the specified position.
+         * @returns {Position} This position, set to the values of the specified position.
          * @throws {ArgumentError} If the specified position is null or undefined.
          */
-        Position.fromPosition = function (position) {
+        Position.setToPosition = function (position) {
             if (!position instanceof Position) {
                 throw new ArgumentError(
                     Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "fromPosition", "missingPosition"));
             }
 
-            return new Position(position.latitude, position.longitude, position.altitude);
+            this.latitude = position.latitude;
+            this.longitude = position.longitude;
+            this.altitude = position.altitude;
+
+            return this;
         };
 
         /**
          * Indicates whether this position is equivalent to a specified position.
          * @param {Position} position The position to compare with this one.
-         * @returns {boolean} <code>true</code> if this position is equivalent to the specified one.
-         * <code>false</code> if the specified position is not equivalent, null, undefined or not a Position.
+         * @returns {boolean} <code>true</code> if this position is equivalent to the specified one, otherwise
+         * <code>false</code>.
          */
         Position.prototype.equals = function (position) {
-            return position instanceof Position
+            return position
                 && position.latitude == this.latitude
                 && position.longitude == this.longitude
                 && position.altitude == this.altitude;
@@ -100,9 +104,10 @@ define([
          * Compute a position along a great circle path at a specified distance between two specified positions.
          * @param {Number} amount The fraction of the path between the two positions at which to compute the new
          * position. This number should be between 0 and 1. If not, it is clamped to the nearest of those values.
-         * @param {Position} position1 The starting location.
-         * @param {Position} position2 The ending location.
-         * @param {Position} result A Location in which to return the result.
+         * @param {Position} position1 The starting position.
+         * @param {Position} position2 The ending position.
+         * @param {Position} result A Position in which to return the result.
+         * @returns {Position} The specified result position.
          * @throws {ArgumentError} If either specified position or the result argument is null or undefined.
          */
         Position.interpolateGreatCircle = function (amount, position1, position2, result) {
@@ -111,9 +116,78 @@ define([
                     Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateGreatCircle", "missingPosition"));
             }
 
+            if (!result) {
+                throw new ArgumentError(
+                    Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateGreatCircle", "missingResult"));
+            }
+
             var t = WWMath.clamp(amount, 0, 1);
-            Location.interpolateGreatCircle(t, position1, position2, result);
             result.altitude = WWMath.interpolate(t, position1.altitude, position2.altitude);
+
+            //noinspection JSCheckFunctionSignatures
+            Location.interpolateGreatCircle(t, position1, position2, result);
+
+            return result;
+        };
+
+        /**
+         * Compute a position along a rhumb path at a specified distance between two specified positions.
+         * @param {Number} amount The fraction of the path between the two positions at which to compute the new
+         * position. This number should be between 0 and 1. If not, it is clamped to the nearest of those values.
+         * @param {Position} position1 The starting position.
+         * @param {Position} position2 The ending position.
+         * @param {Position} result A Position in which to return the result.
+         * @returns {Position} The specified result position.
+         * @throws {ArgumentError} If either specified position or the result argument is null or undefined.
+         */
+        Position.interpolateRhumb = function (amount, position1, position2, result) {
+            if (!position1 || !position2) {
+                throw new ArgumentError(
+                    Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateRhumb", "missingPosition"));
+            }
+
+            if (!result) {
+                throw new ArgumentError(
+                    Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateRhumb", "missingResult"));
+            }
+
+            var t = WWMath.clamp(amount, 0, 1);
+            result.altitude = WWMath.interpolate(t, position1.altitude, position2.altitude);
+
+            //noinspection JSCheckFunctionSignatures
+            Location.interpolateRhumb(t, position1, position2, result);
+
+            return result;
+        };
+
+        /**
+         * Compute a position along a linear path at a specified distance between two specified positions.
+         * @param {Number} amount The fraction of the path between the two positions at which to compute the new
+         * position. This number should be between 0 and 1. If not, it is clamped to the nearest of those values.
+         * @param {Position} position1 The starting position.
+         * @param {Position} position2 The ending position.
+         * @param {Position} result A Position in which to return the result.
+         * @returns {Position} The specified result position.
+         * @throws {ArgumentError} If either specified position or the result argument is null or undefined.
+         */
+        Position.interpolateLinear = function (amount, position1, position2, result) {
+            if (!position1 || !position2) {
+                throw new ArgumentError(
+                    Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateLinear", "missingPosition"));
+            }
+
+            if (!result) {
+                throw new ArgumentError(
+                    Logger.logMessage(Logger.LEVEL_SEVERE, "Position", "interpolateLinear", "missingResult"));
+            }
+
+            var t = WWMath.clamp(amount, 0, 1);
+            result.altitude = WWMath.interpolate(t, position1.altitude, position2.altitude);
+
+            //noinspection JSCheckFunctionSignatures
+            Location.interpolateLinear(t, position1, position2, result);
+
+            return result;
         };
 
         return Position;
