4254cc5c7039b127708e47228328d4358a5651fa
danm <danm@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-12-01 22:21:09 +0000

    Added Version/ID in header.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2498 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WebWorldWind/test/Vec3.test.js
+++ WebWorldWind/test/Vec3.test.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+/**
+ * @version $Id$
+ */
 require({
     baseUrl: '/test/'
 }, [
