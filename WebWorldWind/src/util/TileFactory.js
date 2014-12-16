7f270bdca425e860630eaf6eaf594c4ab9f29d83
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2014-12-16 23:43:51 +0000

    Finished implementation of Tile.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2580 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WebWorldWind/src/util/TileFactory.js
+++ WebWorldWind/src/util/TileFactory.js
@@ -28,7 +28,7 @@ define([
              * Creates a tile for a specified sector, level and row and column within that level.
              * Implementers of this interface must implement this function.
              * @param {Sector} sector The sector the tile spans.
-             * @param {Number} level The level the tile is a member of.
+             * @param {Level} level The level the tile is a member of.
              * @param {Number} row The tile's row within the specified level.
              * @param {Number} column The tile's column within the specified level.
              * @throws {ArgumentError} If the specified sector is null or undefined.
