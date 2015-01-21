6701ed3c6ec04450c85bf2fe7c5427e50fcc265a
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-01-21 02:16:16 +0000

    Modified TileFactory to be documented as a class rather than a module.
    git-svn-id: http://worldwind31.arc.nasa.gov/svn/trunk@2664 822214bb-dcf7-4c8d-a767-17ae0fb20d88

--- WebWorldWind/src/util/TileFactory.js
+++ WebWorldWind/src/util/TileFactory.js
@@ -3,40 +3,39 @@
  * National Aeronautics and Space Administration. All Rights Reserved.
  */
 /**
+ * @exports TileFactory
  * @version $Id$
  */
-define([
-        '../error/ArgumentError',
-        '../util/Level',
-        '../util/Logger',
-        '../geom/Sector',
+define(['../util/Logger',
         '../error/UnsupportedOperationError'
     ],
-    function (Level,
-              Logger,
-              Sector,
+    function (Logger,
               UnsupportedOperationError) {
         "use strict";
 
         /**
+         * Applications must not call this constructor. It is an interface class and is not meant to be instantiated
+         * directly.
+         * @alias TileFactory
+         * @constructor
+         * @classdesc
          * Represents a tile factory.
          * This is an interface class and is not meant to be instantiated directly.
-         * @exports TileFactory
          */
-        var TileFactory = {
-            /**
-             * Creates a tile for a specified sector, level and row and column within that level.
-             * Implementers of this interface must implement this function.
-             * @param {Sector} sector The sector the tile spans.
-             * @param {Level} level The level the tile is a member of.
-             * @param {Number} row The tile's row within the specified level.
-             * @param {Number} column The tile's column within the specified level.
-             * @throws {ArgumentError} If the specified sector is null or undefined.
-             */
-            createTile: function (sector, level, row, column) {
-                throw new UnsupportedOperationError(
-                    Logger.logMessage(Logger.LEVEL_SEVERE, "TileFactory", "createTile", "abstractInvocation"));
-            }
+        var TileFactory = function () {};
+
+        /**
+         * Creates a tile for a specified sector, level and row and column within that level.
+         * Implementers of this interface must implement this function.
+         * @param {Sector} sector The sector the tile spans.
+         * @param {Level} level The level the tile is a member of.
+         * @param {Number} row The tile's row within the specified level.
+         * @param {Number} column The tile's column within the specified level.
+         * @throws {ArgumentError} If the specified sector is null or undefined.
+         */
+        TileFactory.prototype.createTile = function (sector, level, row, column) {
+            throw new UnsupportedOperationError(
+                Logger.logMessage(Logger.LEVEL_SEVERE, "TileFactory", "createTile", "abstractInvocation"));
         };
 
         return TileFactory;
